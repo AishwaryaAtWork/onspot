2aacbcb8baac5784406d3e599d9549b2f5a7a73b
GauravTheBeginner <gaurav8861r@gmail.com>
2024-01-05 04:28:26 +0530

    fixing ui in service section

--- components/services/Services.jsx
+++ components/services/Services.jsx
@@ -7,9 +7,9 @@ function Services() {
         <div className='flex  justify-center gap-1 flex-wrap '>
             {serviceData.map((item, index) => {
                 return <div className=' p-5 text-center flex items-center flex-col gap-1 md:gap-0.5 ' key={index}>
-                    <div className='flex  justify-center'><Image src={item.logo} width={128} alt='' /></div>
-                    <p className='font-inter text-3xl  w-30 font-semibold leading-normal border-[black] '>{item.name}</p>
-                    <p className='lg:w-96 md:w-40 text-center  font-semibold  text-[0.75rem] '>{item.para}</p>
+                    <div className='flex  justify-center'><Image src={item.logo} width={110} alt='' /></div>
+                    <p className='font-inter text-[40px]  w-27 font-semibold leading-normal border-[black] '>{item.name}</p>
+                    <p className='lg:w-96 md:w-auto md:text-[2rem] text-center  font-medium  lg:text-[0.9rem] '>{item.para}</p>
                 </div>
             }) }
         </div>
