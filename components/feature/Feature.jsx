f58b5fabd444f1c1f92b85696ec8db8667d9ea31
GauravTheBeginner <gaurav8861r@gmail.com>
2024-01-06 02:06:28 +0530

    fixing mobile view for this componenet

--- components/feature/Feature.jsx
+++ components/feature/Feature.jsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 import cab from "@/public/images/cab.png"
 function Feature() {
   return (
-    <div className='flex justify-evenly flex-wrap   '>
-        <div className='  w-[40rem] flex flex-col gap-4 '>
-            <p className='font-semibold  text-[3rem] w-[40rem]'>Get immediate intercity cabs at Best Price</p>
+    <div className='flex justify-evenly flex-wrap '>
+        <div className='  w-[40rem]  p-4 flex flex-col gap-4 '>
+            <p className='font-semibold  text-[3rem]'>Get immediate intercity cabs at Best Price</p>
             <p className='text-[1rem]  font-medium leading-normal '>Discover seamless intercity travel solutions at unmatched prices with our prompt and reliable cab services. We offer immediate availability, ensuring you reach your destination hassle-free at the best rates possible. Our skilled drivers, diverse vehicle options, and 24/7 service guarantee a comfortable journey every time. Whether it&apos;s city-to-city travel, airport transfers, tailored packages, corporate needs, or tourist explorations, we&apos;ve got you covered. Booking is easy—call us, book online, or walk in. Contact us now at [Insert Phone Number] or [Insert Email Address] to experience convenience, affordability, and comfort in your intercity travels.</p>
             <div className='flex  gap-8'>
                 <div className='text-[1rem] font-semibold'>
@@ -19,7 +19,7 @@ function Feature() {
             </div>
             <button className='border-2 mt-4 border-[#003C82] w-56 font-medium text-[#003C82] text-2xl rounded-full p-[16px]'>Book Now</button>
         </div>
-        <div className='  '>
+        <div className=' p-4 '>
             <Image className='mt-5 border-2 p-4 rounded-3xl' src={cab}  alt=''/>
         </div>
     </div>
