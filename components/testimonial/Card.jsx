e416da485250fc903bb867da8b5648215953a0f4
GauravTheBeginner <gaurav8861r@gmail.com>
2024-01-06 23:22:20 +0530

    fixing card ui

--- components/testimonial/Card.jsx
+++ components/testimonial/Card.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 function Card({ name, review, avatar }) {
   return (
-    <div className="max-w-xs mx-auto w-full lg:max-w-none lg:w-80 lg:h-80 rounded-2xl flex flex-col gap-8 border-2 border-[#003C82] p-4">
+    <div className="max-w-xs mx-auto w-[84%] lg:max-w-none lg:w-84 lg:h-84 rounded-2xl flex flex-col gap-8 border-2 border-[#003C82] p-4">
       <div className="bg-[#ECF5FF] rounded-lg text-sm font-normal leading-normal p-4">
         <p className="text-gray-800">{review}</p>
       </div>
