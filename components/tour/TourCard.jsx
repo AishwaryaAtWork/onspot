176c4ae5279e9ba9a2a0b6014494c2243f9a15ac
GauravTheBeginner <gaurav8861r@gmail.com>
2024-01-10 05:37:20 +0530

    fixing bug

--- components/tour/TourCard.jsx
+++ components/tour/TourCard.jsx
@@ -3,14 +3,14 @@ import "@/styles/Tour.css"
 function TourCard({location,detail,from,to}) {
     
   return (
-    <div className=' flex flex-wrap justify-center'>
-     <div className='flex justify-center items-center border-2 h-[32rem] p-4 rounded-2xl border-black w-[full]'>
-        <div className='tour  h-[30rem] bg-no-repeat    p-5 flex flex-col justify-between transform transition-all duration-200  hover:scale-105' >
+    <div className=' flex flex-wrap justify-center gap-20'>
+     <div className='flex border-2 h-[32rem] rounded-2xl justify-center items-center w-[90%] p-3   border-black '>
+        <div className='tour  h-[30rem] w-[30rem] bg-no-repeat  rounded-2xl    p-[5%] flex flex-col justify-between transform transition-all duration-200  hover:scale-105' >
             <div className='text-white text-md font-medium'>{location}</div>
             <div className=' flex flex-col gap-7'>
                 <div className='flex flex-col gap-2'>
                     <p className='text-sm text-white'>{detail}</p>
-                    <p className='text-[1.8rem] text-white'>{from} to {to}</p>
+                    <p className='text-[1.8rem] w-[100%] text-white'>{from} to {to}</p>
                 </div>
                 <button className='p-2 w-40 text-2xl mb-4 text-white font-medium border-2 border-white rounded-full'>Book Now</button>
             </div>
