f0e4cdcdc8186d7280394d9645f69e00255bc670
GauravTheBeginner <gaurav8861r@gmail.com>
2024-01-10 10:05:37 +0530

    adding icon

--- components/tour/TourCard.jsx
+++ components/tour/TourCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import "@/styles/Tour.css"
+import locatin from "@/public/images/location.svg"
+import Image from 'next/image'
 function TourCard({location,detail,from,to}) {
     
   return (
     <div className=' flex flex-wrap justify-center gap-20'>
      <div className='flex border-2 h-[32rem] rounded-2xl justify-center items-center w-[90%] p-3   border-black '>
         <div className='tour  h-[30rem] w-[30rem] bg-no-repeat  rounded-2xl    p-[5%] flex flex-col justify-between transform transition-all duration-200  hover:scale-105' >
-            <div className='text-white text-md font-medium'>{location}</div>
+            <div className='text-white text-md font-medium gap-1 flex'> <Image src={locatin} width={13}  height={13} alt=''/> {location}</div>
             <div className=' flex flex-col gap-7'>
                 <div className='flex flex-col gap-2'>
                     <p className='text-sm text-white'>{detail}</p>
