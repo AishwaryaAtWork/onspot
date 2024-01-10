28c6874f011dcb37ba3456e0263e907dcada6215
GauravTheBeginner <gaurav8861r@gmail.com>
2024-01-10 23:45:12 +0530

    fixing font size

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
   return ( 
     <div className='w-full h-[39rem] bg-gradient-to-r from-blue-500 to-purple-500 flex flex-col justify-center items-center  p-16   '>
         <div className=' glass-container z-0 border-2 h-[full] border-black rounded-3xl flex flex-col text-center gap-2 p-8'>
-            <p className='text-[1rem] md:text-[2rem] xl:text-[2rem] font-semibold'>Easy and Cheap Intercity Travelling With Us! Call and Get Your Ride On Spot.</p>
+            <p className='text-[1.5rem] md:text-[2rem] xl:text-[2rem] font-semibold'>Easy and Cheap Intercity Travelling With Us! Call and Get Your Ride On Spot.</p>
             <div> 
                 <p className='text-[1rem] font-medium'>Call and Get Your Ride On Spot.</p>
                 <p className='text-[1rem] font-medium mb-2'>Plan your trip now</p>
