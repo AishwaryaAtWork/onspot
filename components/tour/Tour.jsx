e27bceb729f2e30240543678d0b164d68025ca24
GauravTheBeginner <gaurav8861r@gmail.com>
2024-01-10 09:18:04 +0530

    fixing timing

--- components/tour/Tour.jsx
+++ components/tour/Tour.jsx
@@ -17,7 +17,7 @@ function Tour() {
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 7000,
     beforeChange: (current, next) => {
       setCurrentSlide(next);
      
