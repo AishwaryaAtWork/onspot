12d37d8ff636087469ba638ab4d89896e870c75e
GauravTheBeginner <gaurav8861r@gmail.com>
2024-01-16 03:05:19 +0530

    modified:   app/contact/page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -54,7 +54,7 @@ export default function Example() {
               />
             </div>
           </div>
-          <div className="sm:col-span-2">
+          {/* <div className="sm:col-span-2">
             <label htmlFor="company" className="block text-sm font-semibold leading-6 text-gray-900">
               Company
             </label>
@@ -67,7 +67,7 @@ export default function Example() {
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
-          </div>
+          </div> */}
           <div className="sm:col-span-2">
             <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
               Email
